8d35d92d61cd8de3a40b65d66b5e70f5a2ffbc42
YPanana <33684408+YPanana@users.noreply.github.com>
2017-12-14 16:54:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 import com.oc.modele.*;
 
 public class Main {
 
+	public static void main(String[] args) {
+		Pendu p = new Pendu();
+		
+		System.out.println("Bienvenue dans le pendu !");
+		System.out.println("-------------------------");	
+		p.nouvellePartie();
+	}
 }
