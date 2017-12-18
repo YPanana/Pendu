56d7d6eec648cb2da3f0b4aa1e8020b05c81f06e
YPanana <33684408+YPanana@users.noreply.github.com>
2017-12-18 15:41:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 
 import com.oc.modele.*;
+import com.oc.vue.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Pendu p = new Pendu();
-		
-		System.out.println("Bienvenue dans le pendu !");
-		System.out.println("-------------------------");	
-		p.nouvellePartie();
+		Fenetre fen = new Fenetre();
 	}
 }
